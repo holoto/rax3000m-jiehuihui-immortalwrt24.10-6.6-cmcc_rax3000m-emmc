2859b048934bb06e3b390e28c286c37f76d0203a
holoto <laibin45@gmail.com>
2025-09-12 14:58:06 +0200

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #luci-app-dockerman up
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-git clone https://github.com/sbwml/luci-app-openlist package/openlist
+#git clone https://github.com/sbwml/luci-app-openlist package/openlist
